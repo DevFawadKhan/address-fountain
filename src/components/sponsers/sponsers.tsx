1b09accca5f4ed92f64d96b25b459f2059779a50
DevFawadKhan <mfawadkhan05@gmail.com>
2025-12-18 16:16:26 +0500

    first changes

---
+++ src/components/sponsers/sponsers.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sponsers() {
+  return (
+    <>
+      <div className="bg-black w-[50%] h-24 m-auto mt-40 rounded-xl"></div>
+    </>
+  );
+}
+
+export default Sponsers;
