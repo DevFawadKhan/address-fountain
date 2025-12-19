2cb2d58dbc5a306c43275e3639258b3cfd8cef05
DevBasitali <rajaalix2022@gmail.com>
2025-12-19 15:15:07 +0500

    basit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Sponsers from "@/components/sponsers/sponsers";
+import Hero from "@/components/hero/hero";
+// import About from "@/app/about/page"
 export default function Home() {
   return (
     <>
-      <Sponsers />
+    <Hero/>
+    {/* <About/> */}
+      {/* <Sponsers /> */}
+      
     </>
   );
 }
