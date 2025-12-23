23bf405a6e47072e157cc1e4324e4bf0531c5582
DevBasitali <rajaalix2022@gmail.com>
2025-12-23 17:50:11 +0500

    basit: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-// import Image from "next/image";
 import Sponsers from "@/components/sponsers/sponsers";
 import Hero from "@/components/hero/hero";
 import GuidingPrinciples from "@/components/guiding-principles/principles";
 import AmenitiesSection from "@/components/amanitiesSection/page";
 import Modern from "@/components/modernliving/Modern";
 import SuitesSection from "@/components/suites/SuitesSection";
-// import About from "@/app/about/page"
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <About/> */}
       <Sponsers />
       <GuidingPrinciples />
       <Modern />
-      <SuitesSection />
       <AmenitiesSection />
+      <SuitesSection />
     </>
   );
 }
