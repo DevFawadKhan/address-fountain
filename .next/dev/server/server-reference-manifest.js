6f0c266020beb14196dc5ce3a2774ffc46e2a70e
DevBasitali <rajaalix2022@gmail.com>
2025-12-23 17:42:10 +0500

    chore: remove accidental build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PHsTWOji/dAguVz6Lx+20TKT3yQzmfQCeHKqZtqusUc=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_c24e1de0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_9cd79516._.js",
-  "static/chunks/src_components_a996f548._.js"
-],
-    source: "dynamic"
-});
