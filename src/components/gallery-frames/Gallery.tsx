7e519bec6291ef65e1d6c4df2aab9d400ab56ca2
DevFawadKhan <mfawadkhan05@gmail.com>
2025-12-23 17:22:08 +0500

    addimage tag

--- src/components/gallery-frames/Gallery.tsx
+++ src/components/gallery-frames/Gallery.tsx
@@ -1,97 +1,6 @@
-"use client";
-// import React from "react";
-// import ButtonSecondary from "@/components/ui/ButtonSecondary";
-// function Gallery() {
-//   return (
-//     <>
-//       <section>
-//         <div className="flex justify-center flex-col items-center mt-16">
-//           <h1>Every Frame Tells a Story</h1>
-//           <p className="w-md text-center bg-red-500  2xl:w-lg">
-//             Explore captivating moments and architectural excellence that define
-//             our vision of modern mountain living.
-//           </p>
-//           <ButtonSecondary
-//             className="w-40 font-bold!"
-//             children="Explore All"
-//           ></ButtonSecondary>
-//         </div>
-//         <div className="flex flex-row gap-5 justify-center items-center">
-//           {/* first frame */}
-//           <div className="flex-col flex space-y-5">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-48 lg:h-52! 2xl:h-72!"
-//             />
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-48 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* second frame */}
-//           <div className="mt-40">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-56 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* third  frame */}
-//           <div className="mt-72">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-56 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* four  frame */}
-//           <div className="mt-52">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-56 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* five  frame */}
-//           <div className="mt-72">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-56 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* six  frame */}
-//           <div className="mt-44">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-56 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//           {/* saven */}
-//           <div className="flex-col flex space-y-5">
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-48 lg:h-52! 2xl:h-72!"
-//             />
-//             <img
-//               src="/assets/galleryframes/frame1.png"
-//               alt=""
-//               className="w-48 lg:h-52! 2xl:h-72!"
-//             />
-//           </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Gallery;
 import React from "react";
 import { motion, type Transition } from "framer-motion";
+import Image from "next/image";
 
 import ButtonSecondary from "@/components/ui/ButtonSecondary";
 
@@ -145,19 +54,22 @@ function Gallery() {
           {...floatY(0)}
           className="hidden flex-col lg:flex space-y-5 rounded-md"
         >
-          <img
+          <Image
             src="/assets/galleryframes/frame1.png"
+            alt="frame1"
             className="w-full lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]! rounded-md"
           />
-          <img
+          <Image
             src="/assets/galleryframes/frame2.png"
+            alt="frame2"
             className=" w-full lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]! rounded-md"
           />
         </motion.div>
 
         {/* second */}
         <motion.div {...floatY(0.5)} className=" lg:mt-40">
-          <img
+          <Image
+            alt="frame3"
             src="/assets/galleryframes/frame3.png"
             className="w-[1000px]! h-[220px]! rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
@@ -165,7 +77,8 @@ function Gallery() {
 
         {/* third */}
         <motion.div {...floatY(1)} className="mt-10 md:mt-14 lg:mt-72">
-          <img
+          <Image
+            alt="frame4"
             src="/assets/galleryframes/frame4.png"
             className="w-[1000px]! h-[220px]! rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
@@ -173,7 +86,8 @@ function Gallery() {
 
         {/* fourth */}
         <motion.div {...floatY(1.5)} className=" mt-5 md:mt-2 lg:mt-52">
-          <img
+          <Image
+            alt="frame5"
             src="/assets/galleryframes/frame5.png"
             className="w-[1000px]! h-[220px]! rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
@@ -181,7 +95,8 @@ function Gallery() {
 
         {/* fifth */}
         <motion.div {...floatY(2)} className="mt-10 md:mt-15 lg:mt-72">
-          <img
+          <Image
+            alt="frame5"
             src="/assets/galleryframes/frame6.png"
             className="w-[1000px]! h-[220px]! rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
@@ -189,7 +104,8 @@ function Gallery() {
 
         {/* sixth */}
         <motion.div {...floatY(2.5)} className="lg:mt-44">
-          <img
+          <Image
+            alt="frame7"
             src="/assets/galleryframes/frame7.png"
             className="w-[1000px]! h-[220px]! rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
@@ -200,11 +116,13 @@ function Gallery() {
           {...floatY(3)}
           className="hidden flex-col lg:flex space-y-5"
         >
-          <img
+          <Image
+            alt="frame8"
             src="/assets/galleryframes/frame8.png"
             className="rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
-          <img
+          <Image
+            alt="frame9"
             src="/assets/galleryframes/frame9.png"
             className="rounded-md lg:w-[500px]! lg:h-[230px]! 2xl:h-[300px]!"
           />
