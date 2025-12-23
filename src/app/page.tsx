73893c8d6f4a4375f974e3fd08bc1b08b1e127d0
DevBasitali <rajaalix2022@gmail.com>
2025-12-23 19:25:49 +0500

    basit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import Hero from "@/components/hero/hero";
 import GuidingPrinciples from "@/components/guiding-principles/principles";
 import AmenitiesSection from "@/components/amanitiesSection/page";
 import Modern from "@/components/modernliving/Modern";
-// import About from "@/app/about/page"
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <About/> */}
       <Sponsers />
       <GuidingPrinciples />
       <Modern />
